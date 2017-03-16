62edf4eb91ac02f3c31729ef981cc2a62f05fc65
Paul Bevis <pbevis@console.com.au>
2017-03-16 10:23:03 +1000

    CS-869: Warns on all components that are not created as inline objects.

---
+++ tests/lib/rules/component-defined-inline.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const rule = require("../../../lib/rules/component-defined-inline");
+const RuleTester = require("../../../node_modules/eslint/lib/testers/rule-tester");
+
+const ruleTester = new RuleTester();
+ruleTester.run("component-defined-inline", rule, {
+  valid: [
+    "angular.module('App').component('foo', {});"
+  ],
+  invalid: [
+    {
+      code: "var MyComponent={}; angular.module('App').component('foo', MyComponent);",
+      errors: [{message: "Component not created as in-line anonymous object"}]
+    }
+  ]
+});
