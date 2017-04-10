cdc6b4d4633453518c800c3f26511304ea67ede0
Tom McQuarrie <tmcquarrie@console.com.au>
2017-04-10 12:52:01 +1000

    Review fixes

--- tests/lib/rules/preserve-di-assignment-case.js
+++ tests/lib/rules/preserve-di-assignment-case.js
@@ -2,31 +2,49 @@
 
 const rule = require("../../../lib/rules/preserve-di-assignment-case");
 const RuleTester = require("../../../node_modules/eslint/lib/testers/rule-tester");
+const myConfig = require('./index.js');
+
+RuleTester.setDefaultConfig(myConfig);
 
 const ruleTester = new RuleTester();
 ruleTester.run("preserve-di-assignment-case", rule, {
-    valid: [ `
-class MyClass {
-    constructor(MyService, MyService2) {
-        this.MyService = MyService;
-        this.MyService2 = MyService2;
+  valid: [
+    `class MyTestService1 {
+      constructor(MyService, MyService2) {
+          this.MyService = MyService;
+          this.MyService2 = MyService2;
+      }
     }
-}`],
-    invalid: [
-        {
-            code: `
-class MyClass {
-    constructor(MyService, MyService2, MyService3) {
-        this.nyService = MyService;
-        this.nyService2 = MyService2;
-        this.MyService3 = MyService3;
+    angular.module('App').service('MyTestService', MyTestService1);
+`,
+    `class ConfigInjectService {
+      constructor(CONFIG) {
+          this.TIMEOUT = CONFIG.TIMEOUT;
+      }
     }
-}
-            `,
-            errors: [
-              {message: "Should preserve the import name when assigning to this"},
-              {message: "Should preserve the import name when assigning to this"}
-            ]
+    angular.module('App').service('ConfigInjectService', ConfigInjectService);
+`,
+    `class ConfigInjectService {
+      constructor({ CONFIG, SomethingElse}) {
+          this.something = new Decimal(0);
+      }
+    }
+    angular.module('App').service('ConfigInjectService', ConfigInjectService);
+`],
+  invalid: [{
+    code:
+      `class MyTestService2 {
+        constructor(MyService, MyService2, MyService3) {
+            this.myService = MyService;
+            this.MyCustomServiceHolder = MyService2;
+            this.MyService3 = MyService3;
         }
+      }
+      angular.module('App').service('MyTestService', MyTestService2);
+    `,
+    errors: [
+      {message: "Should preserve the import name when assigning to this"},
+      {message: "Should preserve the import name when assigning to this"}
     ]
+  }]
 });
